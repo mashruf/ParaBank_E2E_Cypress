661ccf7f8aa714719a43f34f5e03d52fd0b83fc5
Mashruf Rohon <mashrufrohon@gmail.com>
2025-06-27 22:14:38 +0600

    updated cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  video: false,
+  screenshotOnRunFailure: true,
   e2e: {
-    baseUrl: "https://parabank.parasoft.com/parabank",
+    baseUrl: 'https://parabank.parasoft.com/parabank',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
+  },
+  env: {
+    allure: true,
   },
 });
