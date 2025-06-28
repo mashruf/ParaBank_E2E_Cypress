9e1d5ce6ce782a1b7a04eba33c11347dd0694c01
Mashruf Rohon <mashrufrohon@gmail.com>
2025-06-28 18:14:10 +0600

    Register with missing field

--- cypress/e2e/UserRegistration.cy.js
+++ cypress/e2e/UserRegistration.cy.js
@@ -20,4 +20,19 @@ describe("Registration",()=>{
         cy.get("div[id='rightPanel']>p").should("contain","Your account was created successfully. You are now logged in.");
     })
 
+    it.only("Register with missing fields",()=>{
+        cy.url().should("include","/register.htm");
+        cy.get("input[value='Register']").click();
+        cy.get("span[id='customer.firstName.errors']").should("contain","First name is required.");
+        cy.get("span[id='customer.lastName.errors']").should("contain","Last name is required.");
+        cy.get("span[id='customer.address.street.errors']").should("contain","Address is required.");
+        cy.get("span[id='customer.address.city.errors']").should("contain","City is required.");
+        cy.get("span[id='customer.address.state.errors']").should("contain","State is required.");
+        cy.get("span[id='customer.address.zipCode.errors']").should("contain","Zip Code is required.");
+        cy.get("tbody tr:nth-child(8) td:nth-child(3)").should("contain","Social Security Number is required.");
+        cy.get("span[id='customer.username.errors']").should("contain","Username is required.");
+        cy.get("span[id='customer.password.errors']").should("contain","Password is required.");
+        cy.get("span[id='repeatedPassword.errors']").should("contain","Password confirmation is required.");
+    })
+
 })
