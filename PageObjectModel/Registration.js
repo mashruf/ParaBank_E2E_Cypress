6005e99a514f9211c6a25294a05a300b873d642b
Mashruf Rohon <mashrufrohon@gmail.com>
2025-06-30 16:40:58 +0600

    Register with existing email

--- PageObjectModel/Registration.js
+++ PageObjectModel/Registration.js
@@ -32,6 +32,20 @@ class Registration {
 
     }
 
+    registerWithExistingUsername(){
+        cy.get("input[id='customer.firstName']").type(this.firstName);
+        cy.get("input[id='customer.lastName']").type(this.lastName);
+        cy.get("input[id='customer.address.street']").type(this.address);
+        cy.get("input[id='customer.address.city']").type(this.city);
+        cy.get("input[id='customer.address.state']").type(this.state);
+        cy.get("input[id='customer.address.zipCode']").type(this.zipCode);
+        cy.get("input[id='customer.phoneNumber']").type(this.phone);
+        cy.get("input[id='customer.ssn']").type(this.ssn);
+        cy.get("input[id='customer.username']").type(this.username);
+        cy.get("input[id='customer.password']").type(this.password);
+        cy.get("#repeatedPassword").type(this.confirmPassword);
+    }
+
     passwordMismatchDuringRegistration() {
         this.userInfo();
         cy.get("input[id='customer.firstName']").type(this.firstName);
--- cypress/e2e/UserRegistration.cy.js
+++ cypress/e2e/UserRegistration.cy.js
@@ -4,42 +4,49 @@ import Registration from "../../PageObjectModel/Registration.js";
 
 const reg = new Registration();
 
-describe("Registration",()=>{
+describe("Registration", () => {
 
-    beforeEach("",()=>{
+    beforeEach("", () => {
         cy.visit("/");
         cy.get("a[href='register.htm']").click();
     })
 
-    it("Registration with valid details",()=>{
+    it("Registration with valid details", () => {
 
-        cy.url().should("include","/register.htm");
+        cy.url().should("include", "/register.htm");
         reg.registrationWithValidDetails();
         cy.get("input[value='Register']").click();
-        cy.get(".title").should("contain",reg.username);
-        cy.get("div[id='rightPanel']>p").should("contain","Your account was created successfully. You are now logged in.");
+        cy.get(".title").should("contain", reg.username);
+        cy.get("div[id='rightPanel']>p").should("contain", "Your account was created successfully. You are now logged in.");
     })
 
-    it("Register with missing fields",()=>{
-        cy.url().should("include","/register.htm");
+    it("Register with missing fields", () => {
+        cy.url().should("include", "/register.htm");
         cy.get("input[value='Register']").click();
-        cy.get("span[id='customer.firstName.errors']").should("contain","First name is required.");
-        cy.get("span[id='customer.lastName.errors']").should("contain","Last name is required.");
-        cy.get("span[id='customer.address.street.errors']").should("contain","Address is required.");
-        cy.get("span[id='customer.address.city.errors']").should("contain","City is required.");
-        cy.get("span[id='customer.address.state.errors']").should("contain","State is required.");
-        cy.get("span[id='customer.address.zipCode.errors']").should("contain","Zip Code is required.");
-        cy.get("tbody tr:nth-child(8) td:nth-child(3)").should("contain","Social Security Number is required.");
-        cy.get("span[id='customer.username.errors']").should("contain","Username is required.");
-        cy.get("span[id='customer.password.errors']").should("contain","Password is required.");
-        cy.get("span[id='repeatedPassword.errors']").should("contain","Password confirmation is required.");
+        cy.get("span[id='customer.firstName.errors']").should("contain", "First name is required.");
+        cy.get("span[id='customer.lastName.errors']").should("contain", "Last name is required.");
+        cy.get("span[id='customer.address.street.errors']").should("contain", "Address is required.");
+        cy.get("span[id='customer.address.city.errors']").should("contain", "City is required.");
+        cy.get("span[id='customer.address.state.errors']").should("contain", "State is required.");
+        cy.get("span[id='customer.address.zipCode.errors']").should("contain", "Zip Code is required.");
+        cy.get("tbody tr:nth-child(8) td:nth-child(3)").should("contain", "Social Security Number is required.");
+        cy.get("span[id='customer.username.errors']").should("contain", "Username is required.");
+        cy.get("span[id='customer.password.errors']").should("contain", "Password is required.");
+        cy.get("span[id='repeatedPassword.errors']").should("contain", "Password confirmation is required.");
     })
 
-    it("Password mismatch during registration",()=>{
-        cy.url().should("include","/register.htm");
+    it("Register with existing username", () => {
+        cy.url().should("include", "/register.htm");
+        reg.registerWithExistingUsername();
+        cy.get("input[value='Register']").click();
+        cy.get("span[id='customer.username.errors']").should("contain","This username already exists.");
+    })
+
+    it("Password mismatch during registration", () => {
+        cy.url().should("include", "/register.htm");
         reg.passwordMismatchDuringRegistration();
         cy.get("input[value='Register']").click();
-        cy.get("span[id='repeatedPassword.errors']").should("contain","Passwords did not match.");
+        cy.get("span[id='repeatedPassword.errors']").should("contain", "Passwords did not match.");
     })
 
 })
